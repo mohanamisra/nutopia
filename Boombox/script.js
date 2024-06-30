2f117f0fa2630e6e903e61df30aad0dad52054cd
Mohana Misra <iammohanamisra@gmail.com>
2024-06-30 13:48:50 +0530

    IT WORKS!

--- Boombox/script.js
+++ Boombox/script.js
@@ -9,3 +9,23 @@ const track = audioContext.createMediaElementSource(audioElement);
 
 // connect the input element "track" to the output or destination
 track.connect(audioContext.destination);
+
+// get the play button
+const playButton = document.querySelector("button");
+playButton.addEventListener("click", (e) => {
+    if(audioContext.state === "suspended")
+        audioContext.resume();
+
+    if(playButton.dataset.playing === "false") {
+        audioElement.play();
+        playButton.dataset.playing = "true";
+    }
+    else if(playButton.dataset.playing === "true") {
+        audioElement.pause();
+        playButton.dataset.playing = "false";
+    }
+}, false);
+
+audioElement.addEventListener("ended", (e) => {
+    playButton.dataset.playing = "false";
+}, false);
