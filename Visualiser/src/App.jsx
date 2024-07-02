8925791431151feaa1cec275df8451aa01fa95c2
Mohana Misra <iammohanamisra@gmail.com>
2024-07-02 18:47:41 +0530

    setting up things the react-way

--- Visualiser/src/App.jsx
+++ Visualiser/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Canvas from "./Canvas.jsx";
 
 const App = () => {
     return (
         <div>
-            
+            <Canvas/>
         </div>
     );
 };
---
+++ Visualiser/src/Canvas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+const Canvas = () => {
+    return (
+        <canvas id = "myCanvas">
+            
+        </canvas>
+    );
+};
+
+export default Canvas;
