38ef7df286254392f54bc497449c383b861c16b1
Mohana Misra <iammohanamisra@gmail.com>
2024-10-05 23:17:22 +0530

    Initial pipe creation

--- PitchyBird/src/App.jsx
+++ PitchyBird/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Pipe from "./components/Pipe.jsx"
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className = "app-container">
+        <Pipe/>
+    </div>
   )
 }
 
---
+++ PitchyBird/src/components/Pipe.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Pipe = () => {
+    const pipeStyle = {
+        container: {
+            width: "100px",
+            height: "100%",
+            backgroundColor: "white",
+        }
+    }
+
+    return (
+        <div className="pipe-container" style={pipeStyle.container}>
+            
+        </div>
+    );
+};
+
+export default Pipe;
